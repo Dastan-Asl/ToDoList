ab82cefe074dbfb4385df817931ca02eadef0d35
Dastan-Asl <63645943+Dastan-Asl@users.noreply.github.com>
2021-09-20 20:19:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Box,
+  VStack,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Button,
+  Checkbox,
+  Text,
+} from "@chakra-ui/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Functional extends React.Component {
+  state = {
+    value: "",
+    tasks: [],
+  };
+
+  handleAddTask = () => {
+    const task = {
+      text: this.state.value,
+      completed: false,
+    };
+    const arr = [...this.state.tasks, task];
+    this.setState({ tasks: arr, value: "" });
+  };
+
+  handleToggleTask = (e, index) => {
+    const check = e.target.checked;
+    const arr = [...this.state.tasks];
+    arr[index].completed = check;
+    this.setState({ tasks: arr });
+  };
+
+  render() {
+    return (
+      <Box p={20}>
+        <VStack alignItems="stretch" spacing={5}>
+          <Box borderWidth={1} p={5} borderRadius="lg">
+            <FormControl id="todo">
+              <FormLabel>What do you want to do?</FormLabel>
+              <HStack>
+                <Input
+                  value={this.state.value}
+                  onChange={(e) => this.setState({ value: e.target.value })}
+                  placeholder="Your next task..."
+                />
+                <Button
+                  disabled={this.state.value.length === 0}
+                  onClick={this.handleAddTask}
+                >
+                  Submit
+                </Button>
+              </HStack>
+            </FormControl>
+          </Box>
+          <Box borderWidth={1} p={5} borderRadius="lg" bg="gray.100">
+            <VStack alignItems="stretch">
+              {this.state.tasks.map((task, index) => {
+                return (
+                  <Box
+                    key={index}
+                    borderWidth="1px"
+                    backgroundColor="white"
+                    borderRadius="lg"
+                    p={3}
+                  >
+                    <HStack spacing={3}>
+                      <Checkbox
+                        size="lg"
+                        onChange={(e) => this.handleToggleTask(e, index)}
+                      />
+                      <Text flex="1" fontSize="lg">
+                        {task.text}
+                      </Text>
+                    </HStack>
+                  </Box>
+                );
+              })}
+            </VStack>
+          </Box>
+        </VStack>
+      </Box>
+    );
+  }
 }
 
-export default App;
+export default Functional;
