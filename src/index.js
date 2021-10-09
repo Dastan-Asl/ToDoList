60c44776dad7f78b6c9eb324df91abefa0e060c6
Dastan-Asl <63645943+Dastan-Asl@users.noreply.github.com>
2021-10-09 19:23:44 +0300

    Added Hush Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import List from "./Functional";
-
+import ListFunc from "./Functional";
+import ListClass from "./Class";
+import { HashRouter, Route, NavLink } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Box, HStack, Button } from "@chakra-ui/react";
+
+const List = () => {
+  return (
+    <Box p={5} borderRadius="lg">
+      <HStack spacing={3}>
+        <NavLink to="/functional">
+          <Button>Functional</Button>
+        </NavLink>
+        <NavLink to="/class">
+          <Button>Class</Button>
+        </NavLink>
+      </HStack>
+    </Box>
+  );
+};
 
 ReactDOM.render(
-  <ChakraProvider>
-    <List />
-  </ChakraProvider>,
+  <HashRouter>
+    <ChakraProvider>
+      <List />
+      <Route path="/functional" component={ListFunc} />
+      <Route path="/class" component={ListClass} />
+    </ChakraProvider>
+  </HashRouter>,
   document.getElementById("root")
 );
